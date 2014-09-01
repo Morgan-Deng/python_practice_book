e7c4ed08df900ee866fc7ae21338f3a203d96a2e
DengLiwen <liwen.morgan.deng@gmail.com>
2014-09-01 10:19:29 +0800

    assignments syntax
    mutiple assignments
    swap values
    practices

---
+++ assignments.py
@@ -0,0 +1,32 @@
+#mutiple assignments
+a, b = 1, 2
+print "a =", a
+print "b =", b
+print "a + b =", a + b
+
+#swapping values
+a, b = 1, 2
+print "a =", a
+print "b =", b
+a, b = b, a
+print "a =", a
+print "b =", b
+
+#swap values practice
+#2, 5
+x = 4
+y = x + 1
+x = 2
+print x, y
+
+#swap values practice
+#6, 4
+x, y = 2, 6
+x, y = y, x + 2
+print x, y
+
+#swap values practice
+#error, because c is not defined
+a, b = 2, 3
+c, b = a, c + 1
+print a, b, c
