954af9184f52d9831c04d43f322b1c2f299996af
DengLiwen <liwen.morgan.deng@gmail.com>
2014-09-01 11:20:42 +0800

    strings
    multi-lines practice

---
+++ strings.py
@@ -0,0 +1,17 @@
+#strings can be enclosed in single quotes or double quotes
+x = "hello"
+y = 'deng'
+print x, y
+
+
+#multi-line strings can be written using three single quotes or three double quotes
+x = """this is a multi-line strings
+    written in 
+    three lines with double three quotes"""
+print x
+
+y = '''multi-line strings can be writter
+    using three single quotes as well.
+    The string can contain 'single quotes' or "double quotes"
+    inside it.'''
+print y
