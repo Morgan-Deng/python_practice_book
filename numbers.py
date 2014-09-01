d5f5cca3210cb84cec01db7430e7a8a4f8ebd01d
DengLiwen <liwen.morgan.deng@gmail.com>
2014-09-01 11:00:01 +0800

    numbers
    practice operators( + , - , * , / , ** )

---
+++ numbers.py
@@ -0,0 +1,77 @@
+#decimal numbers
+print"decimal numbers"
+a = 4.2
+print "a =", a
+b = 2.3
+print "b =", b
+print "a + b =", a + b
+
+
+#addtion
+print "addtion"
+a = 1
+b = 4.2
+print "a =", a
+print "b =", b
+print "a + b =", a + b
+
+
+#subtraction
+print "subtraction"
+a = 1
+b = 4.2
+print "a =", a
+print "b =", b
+print "b - a =", b - a
+
+
+#multiplication
+print "multiplication"
+a = 2
+b = 4.2
+print "a =", a
+print "b =", b
+print "a * b =", a * b
+
+
+#division
+# '/' operator produce only an integer when working on integers.
+print "division"
+a = 2
+b = 4.2
+c = 7
+d = 7.0
+e = 2.0
+print "a =", a
+print "b =", b
+print "c =", c
+print "d =", d
+print "e =", e
+print "b / a =", b / a
+print "c / a =", c / a
+print "d / a =", d / a
+print "b / e =", b / e
+print "c / e =", c / e
+print "d / e =", d / e
+
+
+#exponent
+print "exponent"
+a = 3
+b = 2
+print "a =", a
+print "b =", b
+print "a ** a =", a ** a
+print "b ** b =", b ** b
+print "a ** b =", a ** b
+print "b ** a =", b ** a
+
+
+#remainder
+a = 3
+b = 2
+print "a =", a
+print "b =", b
+print "a % b =", a % b
+print "b % a =", b % a
+
