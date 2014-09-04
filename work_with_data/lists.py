7c2a1ddae149e3797180b7803f0e0738f7e921b2
DengLiwen <liwen.morgan.deng@gmail.com>
2014-09-04 17:30:46 +0800

    Lists
    list comprehensions

--- work_with_data/lists.py
+++ work_with_data/lists.py
@@ -21,18 +21,18 @@ print "b * 3 =", b * 3
 
 # use list slicing to get part of a list
 x = [1, 2, 3, 4]
-# this slice don`t contain the last location
+this slice don`t contain the last location
 print x[0:2]
 print x[1:4]
 print x[0:-1]
-# omitted first index is zero, omitted second index defaults to the size
-# of the list being sliced
+omitted first index is zero, omitted second index defaults to the size
+of the list being sliced
 print x[:]
-# optional third index can be used to specify the increment, which default to 1
+optional third index can be used to specify the increment, which default to 1
 x = range(10)
 print x
 print x[0:6:2]
-# provide -1 increment can reverse a list
+provide -1 increment can reverse a list
 print x[::-1]
 
 
@@ -73,7 +73,7 @@ x = ['a', 'b', 'c']
 y = [1, 2, 3]
 print x, y
 print zip(x, y)
-# it is handy when we want to iterate over two lists together.
+it is handy when we want to iterate over two lists together.
 names = ['a', 'b', 'c']
 values = [1, 2, 3]
 for name, value in zip(names, values):
@@ -87,6 +87,32 @@ def sum(x):
         sum += a
     return sum
 
-# print sum([1, 2, 3])
+print sum([1, 2, 3])
 print sum(['hello', 'world'])
 print sum(['aa', 'bb', 'cc'])
+
+
+# list Comprehensions
+# list comprehensions provide a concise way of creating lists.
+a = range(10)
+print a
+print[x for x in a]
+print[x * x for x in a]
+print[x + 1 for x in a]
+
+# It is possible to filter a list using 'if' inside a list comprehension
+a = range(10)
+print a
+print[x for x in a if x % 2 == 0]
+print[x * x for x in a if x % 2 == 0]
+
+# It is possible to iterate over multiple lists using the built-in
+# function 'zip'.
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+print zip(a, b)
+print[x + y for x, y in zip(a, b)]
+
+# we can use multiple 'for' clause in single list comprehension.
+print[(x, y) for x in range(5) for y in range(5) if (x + y) % 2 == 0]
+print[(x, y) for x in range(5) for y in range(5) if (x + y) % 2 == 0 and x != y]
