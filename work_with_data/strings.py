5b2d12861a19c10fc52fb74baa4289f574970928
DengLiwen <liwen.morgan.deng@gmail.com>
2014-09-04 10:57:20 +0800

    Strings
    simple strings method:
    strip

--- work_with_data/strings.py
+++ work_with_data/strings.py
@@ -12,3 +12,18 @@ x = ' '.join(['hello', 'world'])
 y = ','.join(['a', 'b', 'c'])
 print x
 print y
+
+
+# 'strip' method returns a copy of the given string with leading and
+# trailing whitespace removed
+
+# optionally a string can be passed as argument to remove characters from
+# that string instead of whitespace
+x = ' hello world '
+y = ' abcdefgh'
+print x.strip()
+print y.strip('abdh')
+
+
+
+
